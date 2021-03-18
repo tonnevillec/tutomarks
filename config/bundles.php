64295a7f260becfeb34521dfeb8377c8a6b8c6c4
Cyril Tonneville <tonnevillec@gmail.com>
2021-03-18 21:17:51 +0100

    feat: images pour les tags

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
--- src/Entity/Tags.php
+++ src/Entity/Tags.php
@@ -3,12 +3,14 @@
 namespace App\Entity;
 
 use App\Repository\TagsRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use DateTime;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=TagsRepository::class)
+ * @Vich\Uploadable()
  */
 class Tags
 {
@@ -26,9 +28,21 @@ class Tags
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @var string|null
      */
-    private $logo;
+    private $image;
 
+    /**
+     * @Vich\UploadableField(mapping="tags_images", fileNameProperty="image")
+     * @var File|null
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     * @var DateTime|null
+     */
+    private $updatedAt;
 
 
     public function getId(): ?int
@@ -48,17 +62,44 @@ class Tags
         return $this;
     }
 
-    public function getLogo(): ?string
+    /**
+     * @return string|null
+     */
+    public function getImage(): ?string
     {
-        return $this->logo;
+        return $this->image;
     }
 
-    public function setLogo(?string $logo): self
+    /**
+     * @param string|null $image
+     * @return Tags
+     */
+    public function setImage(?string $image): self
     {
-        $this->logo = $logo;
+        $this->image = $image;
 
         return $this;
     }
+
+    /**
+     * @return File|null
+     */
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageFile(File $image = null): self
+    {
+        $this->imageFile = $image;
+
+        if ($image) {
+            $this->updatedAt = new DateTime('now');
+        }
+
+        return $this;
+    }
+
 
     public function __toString():string
     {
