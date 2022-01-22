701e4e57c6f2d9c99c33550f7f6ede9d8da964ca
Cyril Tonneville <tonnevillec@gmail.com>
2022-01-22 14:57:34 +0100

    fix: affichage de la desc des formations par modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -66,10 +66,4 @@ require('@popperjs/core');
             })
         }
     }
-
-    let tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-
 })();
