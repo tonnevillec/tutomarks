e57d804f1eb266c9b13273a45d75c94e508e44f4
ctonnevi <cyril.tonneville@centralelille.fr>
2022-11-14 12:45:41 +0100

    fix: envoie mail mot de passe oublié

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -3,25 +3,27 @@
 namespace App\Service;
 
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
 
 class EmailService
 {
-    private MailerInterface $mailer;
-
-    public function __construct(MailerInterface $mailer)
-    {
-        $this->mailer = $mailer;
-    }
+    public function __construct(private readonly MailerInterface $mailer)
+    {}
 
     public function send(string $to, string $subject, string $template, array $context = []): void
     {
         $mail = (new TemplatedEmail())
-            ->to($to)
+            ->to(new Address($to))
             ->subject($subject)
             ->htmlTemplate("emails/$template.html.twig")
             ->context($context);
-
-        $this->mailer->send($mail);
+        try {
+            $this->mailer->send($mail);
+        } catch (TransportExceptionInterface $e) {
+            dump($e->getMessage());
+        }
     }
 }
